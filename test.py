6df295e7cd96f1672af0eb132960dca1f44b95ad
testingnewstuff21-dev <testingnewstuff21@gmail.com>
2025-09-29 21:13:45 -0400

    Delete test.py
    this was just a test

--- test.py
+++
@@ -1,3 +0,0 @@
-# this is just to see if I know how a pul request works
-# I will make a change in one of the files and then push it to the remote repository       
-print("This is a test file to check pull requests")
